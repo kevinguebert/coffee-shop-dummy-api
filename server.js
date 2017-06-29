04f710c57a1b5799c56dfaf058d1bb3132feca64
Kevin Guebert <kguebert@sapient.com>
2017-06-29 14:40:58 -0500

    Bugfix - base route was overriding all other routes

--- server.js
+++ server.js
@@ -8,12 +8,13 @@ var port = process.env.PORT || 3000;
 server.use(middlewares);
 
 server.use(jsonServer.rewriter({
-	"/": "/welcome",
-  "/categories/:category": "/categories?id=:category",
+	"/routes": "/welcome",
+  "/categories/:category": "/categories/:category",
   "/categories/:category/:resource": "/:resource?category=:category"
 }));
 
 server.use(router);
+// server.use('/api', router)
 
 server.listen(port, function () {
   console.log('\x1b[36mjson-server is running! PORT: ' + port);
